2c985d6a69281d36141199059f35d37ecb04d030
jayanth <jayanthkumar598@gmail.com>
2024-01-14 07:07:09 +0530

    2nd

--- src/ARRAYS_PRACTISE/Question1.java
+++ src/ARRAYS_PRACTISE/Question1.java
@@ -1,11 +1,28 @@
 package ARRAYS_PRACTISE;
-
+//product of all the numbers in an array
 public class Question1 {
 
 	public static void main(String[] args) {
 
 	
-	System.out.println("hello world");
+		int arr[] = {1,5,6,2,10};
+		
+		int ans = product(arr);
+		
+		System.out.println("answer is "+ans);
+		
+		
+	}
+	
+	public static int product(int arr[])
+	{
+		int ans = 1;
+		for(int i=0;i<arr.length;i++)
+		{
+			ans=ans * arr[i];
+		}
+		
+		return ans;
 	}
 
 }
