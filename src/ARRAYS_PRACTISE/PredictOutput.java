bce480fda51d2d24e7777fa79e2f15240a2fcd38
jayanth <jayanthkumar598@gmail.com>
2024-01-15 08:55:53 +0530

    predict

---
+++ src/ARRAYS_PRACTISE/PredictOutput.java
@@ -0,0 +1,17 @@
+package ARRAYS_PRACTISE;
+
+public class PredictOutput {
+
+	public static void main(String[] args) {
+
+		int sub[] =new int[50];
+		int i;
+		for(i=0;i<=48;i++)
+		{
+			sub[i]=i;
+			System.out.print(sub[i]+"  ");
+		}
+		
+	}
+
+}
